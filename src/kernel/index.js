fddc2c101c13fad9dbbbd136aa40a1f82ed50e6f
jesusqc <jesusquirocampos@gmail.com>
2024-03-29 00:14:09 +0100

    stuff

--- src/kernel/index.js
+++ src/kernel/index.js
@@ -1,28 +1,3 @@
-import LazyModuleLoader from "./features/modules/LazyModuleLoader";
 import IntercordLoader from "./features/loader/IntercordLoader";
-import ModuleSearcher from "./features/modules/ModuleSearcher";
 
 IntercordLoader.initialize();
-
-LazyModuleLoader.waitForStores((userStore, experimentStore) => {
-    enableExperiments(userStore.getCurrentUser(), experimentStore.getSerializedState(), window.FluxDispatcher);
-}, "UserStore", "ExperimentStore");
-
-function enableExperiments(user, serializedState, dispatcher) {
-    try {
-        const m = ModuleSearcher.findByProps("openAlert", "dismissAlert");
-        InterPatcher.addPrefix(m, "openAlert", (data) => {
-            console.log("openAlert was called with data:", data.args);
-        })
-
-        // We add a flag to the user object to indicate that the user is staff
-        user.flags += 1;
-
-        // Then we simulate the OVERLAY_INITIALIZE event to enable experiments
-        dispatcher._actionHandlers._computeOrderedActionHandlers("OVERLAY_INITIALIZE")
-            .filter(e => e.name.includes("Experiment"))
-            .forEach(({actionHandler}) => actionHandler({serializedExperimentStore: serializedState, user,}));
-    } catch (e) {
-        console.log("There was an issue while enabling experiments:", e);
-    }
-}
